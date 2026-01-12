72d379aab9e23b39413c49e9a6b585389da0bc0d
kmcrosby <kcrosby@carthage.edu>
2026-01-11 22:48:45 -0600

    Remove sitemap dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kmcrosby.github.io',
   base: '/crosby-projects',
-  integrations: [
-    mdx(), 
-    sitemap({
-      filter: (page) => !page.includes('/admin/')
-    })
-  ],
+  integrations: [mdx()],
   output: 'static',
 });
